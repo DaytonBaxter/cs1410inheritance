24ef58749e32e865f5b4fb7067d5ed045764d99a
daytonbaxter <baxterd23@gmail.com>
2017-10-17 12:49:09 -0600

    Add doxygen documentation

--- Counter.cpp
+++ Counter.cpp
@@ -3,25 +3,61 @@
 //
 
 #include "Counter.h"
-
+// Doxygen comments Type /*! or /** followed by enter
+/*!
+ * Counter: 1 Parameter constructor
+ * @param count : initial counter
+ */
 Counter::Counter(unsigned int count) : count(count) {}
 
+/*!
+ * Counter: Default Constructor
+ */
 Counter::Counter(): count(0) {}
 
+/**
+ * getCount : Getter for count variable
+ * @return : current count
+ */
 unsigned int Counter::getCount() const
 {
     return count;
 }
 
+/*!
+ * setCount: Set current count
+ * @param count : Number to set counter
+ */
 void Counter::setCount(unsigned int count)
 {
     Counter::count = count;
 }
 
+/*!
+ * ++ Operator: Increments counter
+ * @return : ++counter
+ */
 Counter Counter::operator++() {
     return Counter(++count);
 }
-//constructor
+
+/*!
+ * CountDn -- Operator
+ * @return : Decrements counter
+ */
 Counter CountDn::operator--() {
     return Counter(--count);
 }
+
+/*!
+ * CountDn: Default Constructor
+ */
+CountDn::CountDn() : Counter()
+{
+
+}
+/*!
+ * CountDn 1 paramter Constructor
+ * @param count : Initial Counter
+ */
+CountDn::CountDn(unsigned int count) : Counter(count) {}
